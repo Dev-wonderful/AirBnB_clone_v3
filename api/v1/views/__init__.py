1fce7633049114dd324ef47cbd522bcde5adcdac
Dev-wonderful <adeonederful20@gmail.com>
2023-07-31 17:26:42 +0100

    api status and blueprint

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Modular approach by creating blueprint"""
+from flask import Blueprint
+
+api_views = Blueprint('api_views', __name__, url_prefix='/api/vi')
+
+from api.v1.views.index import *
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""api route created here"""
+from api.v1.views import api_views
+from flask import jsonify
+
+@api_views.route('/status', strict_slashes=False)
+def status():
+    """return the status of the api"""
+    return jsonify({"status": "OK"})
