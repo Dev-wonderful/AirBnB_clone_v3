5b3397cf0d4a1543756cbdfbbba558542e05425e
Dev-wonderful <adeonederful20@gmail.com>
2023-08-01 05:00:00 +0100

    CRUD amenities

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""handle request for amenity views"""
+from flask import jsonify, abort, request
+from api.v1.views import api_views
+from models.amenity import Amenity
+import models
+
+
+@api_views.route('/amenities', strict_slashes=False)
+def get_amenities():
+    """GetS the amenities from the database"""
+    data = []
+    storage = models.storage
+    amenities = storage.all(Amenity).values()
+    # loop through each amenity to convert to dict
+    for amenity in amenities:
+        amenity = amenity.to_dict()
+        data.append(amenity)
+    return jsonify(data)
+
+
+@api_views.route('/amenities/<amenity_id>', strict_slashes=False)
+def get_amenity_by_id(amenity_id):
+    """GetS a amenity from the database"""
+    storage = models.storage
+    # check for presence and return, else throw error
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    amenity = amenity.to_dict()
+    return jsonify(amenity)
+
+
+@api_views.route('/amenities/<amenity_id>', strict_slashes=False, 
+                 methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """delete a amenity from the database, else raise not found error"""
+    storage = models.storage
+    # check for presence and return, else throw error
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    storage.delete(amenity)
+    return jsonify({})
+
+
+@api_views.route('/amenities', strict_slashes=False, methods=['POST'])
+def add_amenity():
+    """adds a amenity to the database"""
+    storage = models.storage
+    # get json data or silently return None if not a json type
+    amenity = request.get_json(silent=True)
+    if amenity is None:
+        return 'Not a JSON', 400
+    # check for presence of required param
+    if amenity.get('name') is None:
+        return 'Missing name', 400
+    storage.new(amenity)
+    storage.save()
+    return amenity, 201
+
+
+@api_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['PUT'])
+def modify_amenity(amenity_id):
+    """modifies a amenity in the database"""
+    storage = models.storage
+    # check for presence and return, else throw error
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    # get json data or silently return None if not a json type
+    amenity_update = request.get_json(silent=True)
+    if amenity_update is None:
+        return 'Not a JSON', 400
+    # check for presence of required param
+    if amenity_update.get('name') is None:
+        return 'Missing name', 400
+    amenity.name = amenity_update.get('name')
+    storage.save()
+    return amenity, 201
